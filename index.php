273a8e3bef43d5c77776a68238f9c85b0931e808
Daniel <33257@ma-web.nl>
2023-09-07 12:14:42 +0200

    9/5 web

--- index.php
+++ index.php
@@ -4,10 +4,13 @@ for($i = 33; $i< 534; $i++){
     
   
 
-     if($i % 2 == 0){
-        echo " <b> $i <br> </b>";
-     }
-     else{
+    if ($i % 5 === 0 && $i % 9 === 0) {
+        echo "web <br>";
+    } 
+    elseif ($i % 2 == 0) {
+    echo " <b> $i <br> </b>";
+    }
+    else{
         echo "$i <br>";
      }
 }
