e26c621b3ac9d4c8f3c94d46e94f7fe1cc198295
Daniel <33257@ma-web.nl>
2023-09-07 12:34:28 +0200

    added bold web

--- index.php
+++ index.php
@@ -3,7 +3,11 @@
 for ($i = 33; $i < 534; $i++) { //make a loop to run through 33 to 534
 
     if ($i % 5 === 0 && $i % 9 === 0) { // if the number is /5 and 9 than you wil echo web
-        echo "web <br>";
+       
+        if ($i % 2 === 0) {//by using this you wil see if the software is a even number and makes it bold
+            echo "<b> web <br> </b>";
+        } else echo "web <br>";
+
     } elseif ($i % 5 === 0) {//if nuber is able to devide by 5 you wil echo softere
         if ($i % 2 === 0) {//by using this you wil see if the software is a even number and makes it bold
             echo "<b> software <br> </b>";
