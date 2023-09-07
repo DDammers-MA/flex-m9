16e9a9f327558f170764fc120a848744c0c11dee
Daniel <33257@ma-web.nl>
2023-09-07 12:26:56 +0200

    added bold software

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <?php
 
-for($i = 33; $i< 534; $i++){
+for($i = 33; $i< 534; $i++){//make a loop to run through 33 to 534
     
-  
-
-    if ($i % 5 === 0 && $i % 9 === 0) {
+    if ($i % 5 === 0 && $i % 9 === 0) {// if the number is /5 and 9 than you wil echo web
         echo "web <br>";
     } 
     elseif ($i % 5 === 0) {
-        echo "software <br>";
+     
+
+        if ($i % 2 === 0){
+            echo "<b> software <br> </b>";
+        }else echo "software <br>";
+       
+
     } elseif ($i % 9 === 0) {
+
         if ($i % 2 === 0){
             echo "<b> developer <br> </b>";
         }else echo "developer <br>";
